d14bdf41e9c9f263046c016cb03bfa5d9572fe18
oleksandrtkach <sanyatkach94@gmail.com>
2019-11-02 16:41:07 +0200

    downgrade home component

---
+++ public/src/app.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {UpgradeModule} from "@angular/upgrade/static";
+import moduleName from './app.module.ajs';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        UpgradeModule
+    ],
+    declarations: [
+        HomeComponent
+    ],
+    entryComponents: [
+        HomeComponent
+    ]
+})
+export class AppModule {
+    constructor(private upgrade: UpgradeModule) {
+    }
+
+    ngDoBootstrap() {
+        this.upgrade.bootstrap(document.documentElement, [moduleName], {strictDi: true})
+    }
+}
